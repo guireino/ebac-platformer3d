c7d89a84761f6e4cc1e766a9cede699672c081ff
Guilherme A. R <guiabucarma@gmail.com>
2024-02-16 14:48:40 -0400

    add jump to prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,13 +6,20 @@ public class Player : MonoBehaviour{
 
     public Rigidbody2D rb;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
-    public float speed;
+    public float speed, forceJump = 2;
 
     // Update is called once per frame
     void Update(){
         
+        HandleJump();
+        HandleMoment();
+    }
+
+
+    private void HandleMoment(){
+
         if(Input.GetKey(KeyCode.LeftArrow)){
            // rb.MovePosition(rb.position - velocity * Time.deltaTime);
            rb.velocity = new Vector2(-speed, rb.velocity.y);
@@ -20,5 +27,21 @@ public class Player : MonoBehaviour{
            // rb.MovePosition(rb.position + velocity * Time.deltaTime);
            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
+
+
+        if(rb.velocity.x > 0){
+            rb.velocity += friction;
+        }else if(rb.velocity.x < 0){
+            rb.velocity -= friction;
+        }
+
     }
+
+    private void HandleJump(){
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = Vector2.up * forceJump;
+        }
+    }
+
 }
