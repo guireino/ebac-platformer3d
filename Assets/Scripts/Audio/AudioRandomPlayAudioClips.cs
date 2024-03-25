c7e4f5d287fd0ca33d227006e14807e09f1bc5da
Guilherme A. R <guiabucarma@gmail.com>
2024-03-25 16:41:32 -0400

    add commit

--- Assets/Scripts/Audio/AudioRandomPlayAudioClips.cs
+++ Assets/Scripts/Audio/AudioRandomPlayAudioClips.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class AudioRandomPlayAudioClips : MonoBehaviour{
 
-    private int _index = 0;
+    private int _index;
     public List<AudioClip> audioClipList;
-    public List<AudioSource> audioSourceList; // criamos 3 AudioSource para pode tocar tres som de um vez, para tocar um 1 por vez
+    public List<AudioSource> audioSourceList; // criamos 3 AudioSource para pode tocar um 1 por vez, vai tem 3 AudioSource um primeiro que vai mudar os audios dos passo
 
     public void PlayRandom(){
 
@@ -17,8 +17,10 @@ public class AudioRandomPlayAudioClips : MonoBehaviour{
         var audioSource = audioSourceList[_index]; // criando variável e atualizando valor da lista de audioClipList
 
         audioSource.clip = audioClipList[Random.Range(0, audioClipList.Count)];
+        Debug.Log("audioSource " + audioSource);
         audioSource.Play();
 
+        Debug.Log("_index " + _index);
         _index++;
     }
     
