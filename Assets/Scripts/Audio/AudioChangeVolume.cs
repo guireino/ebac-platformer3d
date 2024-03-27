c01bc9422d7e52e064702ddea3aef4d6dbbeee58
Guilherme A. R <guiabucarma@gmail.com>
2024-03-27 14:11:01 -0400

    add commit script AudioChangeVolume

--- Assets/Scripts/Audio/AudioChangeVolume.cs
+++ Assets/Scripts/Audio/AudioChangeVolume.cs
@@ -6,9 +6,9 @@ using UnityEngine.Audio;
 public class AudioChangeVolume : MonoBehaviour{
 
     public AudioMixer group;
-    public string floatParam = "myExposeParam";
+    public string floatParam = "myExposeParam"; // Na aula criamos no audio mixer no groups 4 mesa de volume master outra music esta ambience e sfx
 
-    public void ChangeValue(float f){
+    public void ChangeValue(float f){  // para ajustar volume criar duas MyExposedParam um ambience outra sfx
         group.SetFloat(floatParam, f);
     }
 
